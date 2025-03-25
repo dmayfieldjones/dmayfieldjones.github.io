99ee12f268d29944d14bc330b55dee6042cb2a21
Colin <colin.diesh@gmail.com>
2025-03-25 18:31:05 -0400

    Prettier all

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,12 +1,12 @@
-
-
 export default function Contact() {
   return (
     <div>
-    
       <div className="contact-page">
         <h1>Contact Us</h1>
-        <p>You can reach us at: <a href="mailto:dustin@mayfieldjones.com">dustin@mayfieldjones.com</a></p>
+        <p>
+          You can reach us at:{' '}
+          <a href="mailto:dustin@mayfieldjones.com">dustin@mayfieldjones.com</a>
+        </p>
       </div>
     </div>
   )
