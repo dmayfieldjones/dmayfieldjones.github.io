9b3fe1d506ad885d5d08f19808f0b9c4c48945ce
Dustin <dustin@illinois.edu>
2025-03-25 16:18:08 -0500

    navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './index.css'
+import './navbar.css'
 
 export const metadata = {
   title: '7Sisters Farm',
@@ -13,6 +14,17 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <nav className="navbar">
+          <ul className="navbar-menu">
+            <li><a href="/">Home</a></li>
+            <li><a href="/about">About</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li><a href="/BreedingPhilosophy">Breeding Philosophy</a></li>
+            <li><a href="/Litters">Litters</a></li>
+            <li><a href="/Resources">Resources</a></li>
+            <li><a href="/CommonQuestions">Common Questions</a></li>
+          </ul>
+        </nav>
         <div className="wrapper">{children}</div>
       </body>
     </html>
