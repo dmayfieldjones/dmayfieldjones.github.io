15cfd4620cce7d0b7eb6335eae78699a16d5db6d
Colin <colin.diesh@gmail.com>
2025-05-22 18:11:16 -0400

    Misc

--- app/GreatDaneGenomeBrowser/Ideogram.tsx
+++ app/GreatDaneGenomeBrowser/Ideogram.tsx
@@ -1,30 +1,15 @@
-// @ts-nocheck
 'use client'
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 
 import Ideogram from 'ideogram'
 
-export default class ReactIdeogram extends Component {
-  createIdeogram() {
-    const config = Object.assign(
-      {
-        dataDir: 'https://eweitz.github.io/ideogram/dist/data/bands/native/',
-        container: '#ideo-container',
-      },
-      this.props,
-    )
-    new Ideogram(config)
-  }
-
-  componentDidUpdate() {
-    return this.createIdeogram()
-  }
-
-  componentDidMount() {
-    return this.createIdeogram()
-  }
-
-  render() {
-    return <div id="ideo-container"></div>
-  }
+export default function ReactIdeogram(props: Record<string, unknown>) {
+  useEffect(() => {
+    new Ideogram({
+      ...props,
+      dataDir: 'https://eweitz.github.io/ideogram/dist/data/bands/native/',
+      container: '#ideo-container',
+    })
+  }, [props])
+  return <div id="ideo-container"></div>
 }
